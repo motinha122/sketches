cd61b9fe9c72676dcfd4c2f99141634f670def3e
motinha122 <timota132@gmail.com>
2022-11-03 18:38:20 -0300

    sketch 5

--- sketch-04.js
+++ sketch-04.js
@@ -3,7 +3,7 @@ const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [2048, 2048]
+  dimensions: [ 2048, 2048 ]
 };
 
 const sketch = () => {
@@ -12,35 +12,25 @@ const sketch = () => {
     context.fillRect(0, 0, width, height);
     context.fillStyle = 'black';
 
-    const cx = width * 0.5;
-    const cy = height * 0.5;
+    const cx = 0;
+    const cy = height;
     const w = width * 0.01;
     const h = height * 0.1;
 
-    const num = 40;
+    const num = 100;
 
-    const radius = width * 0.3;
+    const radius = width*0.8;
 
     for (let i = 0; i < num; i++) {
       const slice = math.degToRad(360) / num;
       const angle = slice * i;
 
-      x = cx + radius * Math.sin(angle);
-      y = cy + radius * Math.cos(angle);
-
-      context.save();
-      context.translate(x, y);
-      context.rotate(-angle);
-      context.scale(random.range(0.5,2),random.range(0.2,0.5));
-      context.fillRect(-w * 0.5,random.range(0,-h*0.5), w, h);
-      context.restore();      
-
       context.save();
       context.translate(cx,cy);
       context.rotate(-angle);
-      context.lineWidth = random.range(5,20);
+      context.lineWidth = random.range(10,50);
       context.beginPath();
-      context.arc(0,0,radius * random.range(0.7,1.3),slice * random.range(1,-8),slice* random.range(1,5));
+      context.arc(0,0,radius*random.range(0.7,1.3),slice*random.range(1,-8),slice*random.range(1,5));
       context.stroke();
       context.restore();
     }
---
+++ sketch-05.js
@@ -0,0 +1,16 @@
+const canvasSketch = require('canvas-sketch');
+const math = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+
+const settings = {
+  dimensions: [ 2048, 2048 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+  };
+};
+
+canvasSketch(sketch, settings);
